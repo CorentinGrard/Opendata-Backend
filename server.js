fcffd1ed1f5403662479ceaf7616575d1f06ea94
Matthieu Tinnes <matthieu.tinnes@mines-ales.org>
2020-03-23 15:42:31 +0100

    Add swagger

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const bodyParser = require('body-parser');
 const errorHandler = require('_helpers/error-handler');
 const winston = require('./_helpers/winston');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+ 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(morgan('combined', { stream: winston.stream }));
 
 app.use(bodyParser.urlencoded({
@@ -18,6 +23,7 @@ app.use(cors());
 // api routes
 app.use('/api/v1/gsm', require('./controllers/gsm.controller'));
 app.use('/api/v1/fiber', require('./controllers/fiber.controller'));
+
 
 // global error handler
 app.use(errorHandler);
